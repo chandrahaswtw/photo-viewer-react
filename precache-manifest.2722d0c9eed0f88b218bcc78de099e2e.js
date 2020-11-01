0e29f1936e0ee8186bf319e41a4cc7abf42d1d08
chandrahaswtw <chandrahaswtw@gmail.com>
2020-11-01 11:00:30 +0530

    Updates

---
+++ precache-manifest.2722d0c9eed0f88b218bcc78de099e2e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bbf63fc8d51fa8db42979464bbba998",
+    "url": "/photo-viewer-react/index.html"
+  },
+  {
+    "revision": "d831fa0c6aaf07a7dfa6",
+    "url": "/photo-viewer-react/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "086fc3fc4df93119dd66",
+    "url": "/photo-viewer-react/static/css/main.8945347a.chunk.css"
+  },
+  {
+    "revision": "d831fa0c6aaf07a7dfa6",
+    "url": "/photo-viewer-react/static/js/2.822e2b93.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/photo-viewer-react/static/js/2.822e2b93.chunk.js.LICENSE"
+  },
+  {
+    "revision": "086fc3fc4df93119dd66",
+    "url": "/photo-viewer-react/static/js/main.8a2bf21b.chunk.js"
+  },
+  {
+    "revision": "87ac86ef2c1cd686ee5a",
+    "url": "/photo-viewer-react/static/js/runtime-main.5591c81e.js"
+  },
+  {
+    "revision": "2c476f8d2f25c702504d543d3fe50a24",
+    "url": "/photo-viewer-react/static/media/B612-Bold.2c476f8d.ttf"
+  },
+  {
+    "revision": "e8ed14e1cdeed375c3c22bc1c4424b6f",
+    "url": "/photo-viewer-react/static/media/B612-Regular.e8ed14e1.ttf"
+  },
+  {
+    "revision": "8032e501feba47eccf82413d11a4f4e5",
+    "url": "/photo-viewer-react/static/media/Spinner_loader.8032e501.gif"
+  }
+]);
